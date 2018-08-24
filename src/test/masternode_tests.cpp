dc9f7f8eba0910e794b5ab0f9a830e1b2253379a
edthefifth <ed@atlaschain.io>
2018-08-24 14:24:12 -0500

    add masternode unit tests

--- src/test/masternode_tests.cpp
+++ src/test/masternode_tests.cpp
@@ -2,20 +2,45 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "wallet.h"
+#include "walletdb.h"
+#include "chainparams.h"
+#include "masternode.h"
+
 #include <boost/test/unit_test.hpp>
+
+
 
 BOOST_AUTO_TEST_SUITE(masternode_tests)
 
-
-
-BOOST_AUTO_TEST_CASE(masternode_setup)
+void CheckStatus(std::string stExpected , bool unitTest, string walletFile)
 {
-    // Test setup on TestNet
+    
+    CWalletDB walletdb(walletFile, "cr+");
+    CWallet wallet(walletFile);
+
+    CMasternode mn = CMasternode();
+    mn.unitTest = unitTest;
+    std::string stActual = mn.GetStatus();
+    
+    BOOST_CHECK_MESSAGE(stActual == stExpected, strprintf("Incorrect Status: %s != %s",stActual.c_str(),stExpected.c_str()));
+}
+
+
+BOOST_AUTO_TEST_CASE(masternode_status_test)
+{        
+
+    SelectParams(CBaseChainParams::UNITTEST);
+    string unitWalletFile = "unittestwallet.dat";    
+    std::string  expected = "ENABLED";
+    CheckStatus(expected,true,unitWalletFile);
+    
     SelectParams(CBaseChainParams::TESTNET);
-
-    // Test setup on MainNet
-    SelectParams(CBaseChainParams::MAIN);
-
+    string strWalletFile = "wallet.dat";
+    CheckStatus(expected,false,strWalletFile);
+    
+    SelectParams(CBaseChainParams::MAINNET);
+    CheckStatus(expected,false,strWalletFile);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
---
+++ src/test/rpc_masternode_tests.cpp
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "rpcserver.h"
+#include "rpcclient.h"
+#include "key.h"
+#include "base58.h"
+#include "wallet.h"
+#include "uint256.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <univalue.h>
+
+using namespace std;
+
+extern UniValue createArgs(int nRequired, const char* address1 = NULL, const char* address2 = NULL);
+extern UniValue CallRPC(string args);
+
+extern CWallet* pwalletMain;
+
+BOOST_AUTO_TEST_SUITE(rpc_masternode_tests)
+        
+    BOOST_AUTO_TEST_CASE(rpc_masternode)
+    {
+        LOCK(pwalletMain->cs_wallet);
+    } 
+
+
+    BOOST_AUTO_TEST_CASE(rpc_masternode_budget)
+    {
+        LOCK(pwalletMain->cs_wallet);
+    } 
+
+BOOST_AUTO_TEST_SUITE_END()
